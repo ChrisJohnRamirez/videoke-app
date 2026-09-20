1d8359f1632cd37482432e237c077ab7b2a636c2
ChrisJohnRamirez <142198577+ChrisJohnRamirez@users.noreply.github.com>
2026-09-20 23:25:16 +0800

    Add files via upload

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import kantahanLogo from "../assets/branding/kantahanLogo.png"
+import kantahanLogo from "../assets/branding/kantahan-wordmark.png"
 
 type HeaderProps = {
   title: string
@@ -10,7 +10,7 @@ function Header({ title }: HeaderProps) {
     <header className="w-full">
       <div className="mx-auto flex max-w-5xl items-center px-6 py-5">
         <Link to="/" className="font-semibold">
-          <img src={kantahanLogo} alt="KantaHan Logo" className="h-10 w-10 headerLogo" />
+          <img src={kantahanLogo} alt="KantaHan Logo" className="h-10 w-auto object-contain" />
         </Link>
 
         <div className="ml-auto text-lg font-semibold">
